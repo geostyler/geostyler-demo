9889d133f783619183876c5c55b7209f320d45e3
KaiVolland <kaivolland@yahoo.de>
2020-01-17 09:35:28 +0000

    Deploying to gh-pages from  489b849a189bbdd010bcb809383c00cd784865cc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0c363e6c7225c3bb2d8544df079be87b.js"
+  "./precache-manifest.f71d80e00beddd67991a1749a15d500c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
