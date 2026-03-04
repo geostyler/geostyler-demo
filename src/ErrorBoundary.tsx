ff602149712fd5d267953b1a6eb83c32b462eed6
Kai Volland <kaivolland@yahoo.de>
2026-03-04 11:57:15 +0100

    fix: update dependencies and add ErrorBoundary

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, ErrorInfo } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+  description?: string;
+  title?: string;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    console.error("Error caught by ErrorBoundary:", error, info);
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/LanguageSwitcher.tsx
+++ src/LanguageSwitcher.tsx
@@ -22,89 +22,95 @@ export const LanguageSwitcher: FC<LanguageSwitcherProps> = ({ onChange }) => {
     moment.locale(lang);
     switch (lang) {
       case 'en':
-          onChange({
-              codeEditor: 'Code Editor',
-              cardLayout: 'CardLayout (Beta)',
-              examples: 'Examples',
-              graphicalEditor: 'Graphical Editor',
-              language: 'Language',
-              legend: 'Legend',
-              splitView: 'Split View',
-              previewMap: 'Preview Map',
-              loadedSuccess: 'Loaded successfully!',
-              previewMapDataProjection: 'Projection of sample data is expected to be EPSG:4326'
-          }, GsLocale.en_US);
-          break;
-        case 'de':
-          onChange({
-              codeEditor: 'Code Editor',
-              cardLayout: 'CardLayout (Beta)',
-              examples: 'Beispiele',
-              graphicalEditor: 'Grafischer Editor',
-              language: 'Sprache',
-              legend: 'Legende',
-              splitView: 'Geteilte Vorschau',
-              previewMap: 'Vorschau Karte',
-              loadedSuccess: 'Erfolgreich geladen!',
-              previewMapDataProjection: 'Die Beispieldaten werden in der Projektion EPSG:4326 erwartet.'
-          }, GsLocale.de_DE);
-          break;
-        case 'es':
-          onChange({
-              codeEditor: 'Editor de código',
-              cardLayout: 'CardLayout (Beta)',
-              examples: 'Ejemplos',
-              graphicalEditor: 'Editor gráfico',
-              language: 'Idioma',
-              legend: 'Leyenda',
-              splitView: 'Vista partida',
-              previewMap: 'Mapa de previsualización',
-              loadedSuccess: 'Cargado con éxito!',
-              previewMapDataProjection: 'Los datos de la muestra se esperan en la proyección EPSG:4326.'
-          }, GsLocale.es_ES);
-          break;
-        case 'fr':
-          onChange({
-              codeEditor: 'Éditeur de code',
-              cardLayout: 'CardLayout (Beta)',
-              examples: 'Exemples',
-              graphicalEditor: 'Éditeur graphique',
-              language: 'Langue',
-              loadedSuccess: 'Chargement réussi!',
-              legend: 'Légende',
-              splitView: 'Vue partagée',
-              previewMap: 'Carte de prévisualisation',
-              previewMapDataProjection: 'Les données d\'exemple sont attendues dans la projection EPSG:4326.'
-          }, GsLocale.fr_FR);
-          break;
-        case 'ch-zn':
-          onChange({
-              codeEditor: '代码编辑器',
-              cardLayout: 'CardLayout (Beta)',
-              examples: '例子',
-              graphicalEditor: '图形编辑器',
-              language: '语言',
-              legend: 'Legend',
-              splitView: 'Split View',
-              previewMap: '预览图',
-              loadedSuccess: '成功加载',
-              previewMapDataProjection: '预计样本数据将在EPSG:4326的预测中出现。'
-          }, GsLocale.zh_CN);
-          break;
-        default:
-          onChange({
-              codeEditor: 'Code Editor',
-              cardLayout: 'CardLayout (Beta)',
-              examples: 'Examples',
-              graphicalEditor: 'Graphical Editor',
-              language: 'Language',
-              legend: 'Legend',
-              splitView: 'Split View',
-              previewMap: 'Preview Map',
-              loadedSuccess: 'Loaded successfully!',
-              previewMapDataProjection: 'string'
-          }, GsLocale.en_US);
-          break;
+        onChange({
+          codeEditor: 'Code Editor',
+          cardLayout: 'CardLayout',
+          examples: 'Examples',
+          graphicalEditor: 'Graphical Editor',
+          language: 'Language',
+          legend: 'Legend',
+          splitView: 'Split View',
+          previewMap: 'Preview Map',
+          loadedSuccess: 'Loaded successfully!',
+          previewMapDataProjection: 'Projection of sample data is expected to be EPSG:4326',
+          errorBoundaryDescription: 'Invalid geostyler-style. Can\'t render',
+        }, GsLocale.en_US);
+        break;
+      case 'de':
+        onChange({
+          codeEditor: 'Code Editor',
+          cardLayout: 'CardLayout (Beta)',
+          examples: 'Beispiele',
+          graphicalEditor: 'Grafischer Editor',
+          language: 'Sprache',
+          legend: 'Legende',
+          splitView: 'Geteilte Vorschau',
+          previewMap: 'Vorschau Karte',
+          loadedSuccess: 'Erfolgreich geladen!',
+          previewMapDataProjection: 'Die Beispieldaten werden in der Projektion EPSG:4326 erwartet.',
+          errorBoundaryDescription: 'Ungültiger geostyler-style. Rendern nicht möglich.',
+        }, GsLocale.de_DE);
+        break;
+      case 'es':
+        onChange({
+          codeEditor: 'Editor de código',
+          cardLayout: 'CardLayout (Beta)',
+          examples: 'Ejemplos',
+          graphicalEditor: 'Editor gráfico',
+          language: 'Idioma',
+          legend: 'Leyenda',
+          splitView: 'Vista partida',
+          previewMap: 'Mapa de previsualización',
+          loadedSuccess: 'Cargado con éxito!',
+          previewMapDataProjection: 'Los datos de la muestra se esperan en la proyección EPSG:4326.',
+          errorBoundaryDescription: 'geostyler-style no válido. No se puede renderizar.',
+        }, GsLocale.es_ES);
+        break;
+      case 'fr':
+        onChange({
+          codeEditor: 'Éditeur de code',
+          cardLayout: 'CardLayout (Beta)',
+          examples: 'Exemples',
+          graphicalEditor: 'Éditeur graphique',
+          language: 'Langue',
+          loadedSuccess: 'Chargement réussi!',
+          legend: 'Légende',
+          splitView: 'Vue partagée',
+          previewMap: 'Carte de prévisualisation',
+          previewMapDataProjection: 'Les données d\'exemple sont attendues dans la projection EPSG:4326.',
+          errorBoundaryDescription: 'geostyler-style non valide. Impossible de rendre.',
+        }, GsLocale.fr_FR);
+        break;
+      case 'ch-zn':
+        onChange({
+          codeEditor: '代码编辑器',
+          cardLayout: 'CardLayout (Beta)',
+          examples: '例子',
+          graphicalEditor: '图形编辑器',
+          language: '语言',
+          legend: 'Legend',
+          splitView: 'Split View',
+          previewMap: '预览图',
+          loadedSuccess: '成功加载',
+          previewMapDataProjection: '预计样本数据将在EPSG:4326的预测中出现。',
+          errorBoundaryDescription: '无效的geostyler-style。无法渲染。',
+        }, GsLocale.zh_CN);
+        break;
+      default:
+        onChange({
+          codeEditor: 'Code Editor',
+          cardLayout: 'CardLayout (Beta)',
+          examples: 'Examples',
+          graphicalEditor: 'Graphical Editor',
+          language: 'Language',
+          legend: 'Legend',
+          splitView: 'Split View',
+          previewMap: 'Preview Map',
+          loadedSuccess: 'Loaded successfully!',
+          previewMapDataProjection: 'string',
+          errorBoundaryDescription: 'Invalid geostyler-style. Can\'t render',
+        }, GsLocale.en_US);
+        break;
     }
   };
 
